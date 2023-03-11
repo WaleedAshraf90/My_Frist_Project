e009371935e34ac5f252353c36628130beab8f6e
Waleed Ashraf <waleedashraf931@gmail.com>
2023-03-11 22:24:44 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+     
+    double Length, Width;
+    cout << "Enter the Length :";
+    cin >> Length;
+    cout << "Enter the Width : ";
+    cin >> Width;
+    cout << "The area is: " << Length*Width << endl;
+    system("pause");
+    return 0;
+
+    
+}
